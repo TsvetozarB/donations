bfc606461df168c3d06eb6cf08dbf1fcdd9e5b0b
TsvetozarB <tsvetozar.barabashki@gmail.com>
2021-03-08 01:10:43 +0200

    add Page Not Found

---
+++ src/components/pages/NotFoundPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Page not found! Page not found!</h2>
+    <p>
+      You maybe want to see our
+      <router-link to="/donate">donation page</router-link>.
+    </p>
+  </div>
+</template>
+<style scoped>
+div {
+  margin-top: 100px;
+  text-align: center;
+}
+
+a {
+  text-decoration: none;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -9,17 +9,19 @@ import BlogPage from './components/pages/BlogPage.vue';
 import LearnMorePage from './components/pages/LearnMorePage.vue';
 import DonatePage from './components/pages/DonatePage.vue';
 import DonateItemPage from './components/pages/DonateItemPage.vue';
+import NotFoundPage from './components/pages/NotFoundPage.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/about', component: AboutPage },
     { path: '/', component: MainPage },
+    { path: '/about', component: AboutPage },
     { path: '/contacts', component: ContactsPage },
     { path: '/blog', component: BlogPage },
     { path: '/learn-more', component: LearnMorePage },
     { path: '/donate', component: DonatePage },
     { path: '/donate/:itemId', component: DonateItemPage },
+    { path: '/:notFound(.*)', component: NotFoundPage },
   ],
   linkActiveClass: 'nav-active',
   linkExactActiveClass: 'nav-exact-active',
